aefd8f964f1404473aaee181b847b6a3dbf0ef1e
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2015-04-16 12:49:39 -0500

    cambio de tema de acuerdo a la version de api

--- build/generated/source/r/debug/com/codpaa/R.java
+++ build/generated/source/r/debug/com/codpaa/R.java
@@ -453,6 +453,8 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
         public static final int TextShadow=0x7f070002;
