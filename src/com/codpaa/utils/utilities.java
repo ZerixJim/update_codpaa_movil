4902e507de8c7d332a63c797eadd897450dbf216
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2015-05-27 13:15:11 -0500

    correccion de la clase utils

--- src/com/codpaa/utils/utilities.java
+++ src/com/codpaa/utils/utilities.java
@@ -1,6 +1,6 @@
 package com.codpaa.utils;
 
-public class utilities {
+public class Utilities {
     static final String SERVER_IP = "162.144.139.133";
     static final String SERVER_IP_2 = "162.144.139.31";
     static final String BASE_URL = "http://plataformavanguardia.net";
