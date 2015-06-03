9e3b2dedac5fd85cafb5887cac1b63c6029c213c
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2015-06-03 17:56:20 -0500

    cambio de string de la version 31

--- build/generated/source/buildConfig/release/com/codpaa/BuildConfig.java
+++ build/generated/source/buildConfig/release/com/codpaa/BuildConfig.java
@@ -8,10 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.codpaa";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-<<<<<<< HEAD
-  public static final int VERSION_CODE = 30;
-=======
-  public static final int VERSION_CODE = 29;
->>>>>>> experimental
+  public static final int VERSION_CODE = 31;
   public static final String VERSION_NAME = "";
 }
