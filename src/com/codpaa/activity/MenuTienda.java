a1e8ed323eb184ef5c6cbd938d3a43ddabfe74f5
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2018-07-30 11:23:03 -0500

    cambios en la leyenda de precio

--- src/com/codpaa/activity/MenuTienda.java
+++ src/com/codpaa/activity/MenuTienda.java
@@ -293,7 +293,7 @@ public class MenuTienda extends AppCompatActivity implements OnClickListener, Me
 
             final MenuTiendaModel item6 = new MenuTiendaModel();
             item6.setIdMenu(6);
-            item6.setNombreMenu("Precio");
+            item6.setNombreMenu("Inteligencia de mercado \n(Precios propios y competencias)");
             item6.setImage("ic_assessment_grey_600_24dp");
             array.add(item6);
 
