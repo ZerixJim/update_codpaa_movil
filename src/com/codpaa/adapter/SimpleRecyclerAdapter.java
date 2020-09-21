257aab4d839e395d796a26fa49cd18aeaf48dbf7
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2020-09-21 17:23:31 -0500

    garbage code

--- src/com/codpaa/adapter/SimpleRecyclerAdapter.java
+++ src/com/codpaa/adapter/SimpleRecyclerAdapter.java
@@ -43,28 +43,17 @@ public class SimpleRecyclerAdapter extends RecyclerView.Adapter<SimpleRecyclerAd
 
     private List<RutaDia> rutaDias;
     private List<RutaDia> rutasFilter;
-    private Boolean isHomeList = false;
 
-    private static List<String> homeActivitiesList = new ArrayList<>();
-    private static List<String> homeActivitiesSubList = new ArrayList<>();
+
     private Context context;
     private int idPromotor;
 
 
 
-    public void setHomeActivitiesList(Context context) {
-        String[] listArray = context.getResources().getStringArray(R.array.home_activities);
-        String[] subTitleArray = context.getResources().getStringArray(R.array.home_activities_subtitle);
-        for (int i = 0; i < listArray.length; ++i) {
-            homeActivitiesList.add(listArray[i]);
-            homeActivitiesSubList.add(subTitleArray[i]);
-        }
-    }
-
 
 
     public SimpleRecyclerAdapter(Context context, List<RutaDia> rutaDias, int idPromotor) {
-        isHomeList = false;
+
 
         this.context = context;
         this.rutaDias = rutaDias;
@@ -82,16 +71,12 @@ public class SimpleRecyclerAdapter extends RecyclerView.Adapter<SimpleRecyclerAd
 
     @Override
     public void onBindViewHolder(DiaViewHolder dia, int i) {
-        if (isHomeList) {
-            dia.title.setText(homeActivitiesList.get(i));
-            dia.subTitle.setText(homeActivitiesSubList.get(i));
-        } else {
-            final RutaDia diaModel = rutasFilter.get(i);
-            dia.title.setText(diaModel.getNombreTienda());
-            dia.subTitle.setText(diaModel.getSucursal());
-            dia.rol.setText(diaModel.getRol());
-            dia.hora.setText(diaModel.getHora());
-            dia.number.setText(String.format(Locale.getDefault(),"%d", i + 1));
+        final RutaDia diaModel = rutasFilter.get(i);
+        dia.title.setText(diaModel.getNombreTienda());
+        dia.subTitle.setText(diaModel.getSucursal());
+        dia.rol.setText(diaModel.getRol());
+        dia.hora.setText(diaModel.getHora());
+        dia.number.setText(String.format(Locale.getDefault(),"%d", i + 1));
 
             /*if (diaModel.getModo() == 1){
                 dia.modo.setText("promotoria");
@@ -99,80 +84,77 @@ public class SimpleRecyclerAdapter extends RecyclerView.Adapter<SimpleRecyclerAd
                 dia.modo.setText("impulsor");
             }*/
 
-            //Picasso.get().load(Utilities.FORMATOS_PATH + diaModel.getFormato() + ".png").into(dia.image);
+        //Picasso.get().load(Utilities.FORMATOS_PATH + diaModel.getFormato() + ".png").into(dia.image);
 
-            Glide.with(context)
-                    .load(Utilities.FORMATOS_PATH + diaModel.getFormato() + ".png")
-                    .error(R.drawable.ic_broken_image_grey_700_24dp)
-                    .into(dia.image);
-
-
-
-            if (!diaModel.getLongitud().isEmpty() && !diaModel.getLatitud().isEmpty()){
+        Glide.with(context)
+                .load(Utilities.FORMATOS_PATH + diaModel.getFormato() + ".png")
+                .error(R.drawable.ic_broken_image_grey_700_24dp)
+                .into(dia.image);
 
 
-                dia.ubicacion.setVisibility(View.VISIBLE);
-                dia.ubicacion.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri.Builder builder = new Uri.Builder();
-                        builder.scheme("https")
-                                .authority("maps.googleapis.com")
-                                .appendPath("maps")
-                                .appendPath("api")
-                                .appendPath("staticmap")
-                                .appendQueryParameter("center", diaModel.getLatitud() + "," + diaModel.getLongitud())
-                                .appendQueryParameter("zoom", "19")
-                                .appendQueryParameter("scale", "false")
-                                .appendQueryParameter("format", "png")
-                                .appendQueryParameter("key", "AIzaSyCDb3AWOIzMkm-_tdCezft9_ygxrgZ3__0")
-                                .appendQueryParameter("markers", "color:orange|" +
-                                        "label:T|"+ diaModel.getLatitud() + "," +
-                                        diaModel.getLongitud())
-                                .appendQueryParameter("size", "500x800");
 
-                        String url = builder.build().toString();
+        if (!diaModel.getLongitud().isEmpty() && !diaModel.getLatitud().isEmpty()){
+
+
+            dia.ubicacion.setVisibility(View.VISIBLE);
+            dia.ubicacion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri.Builder builder = new Uri.Builder();
+                    builder.scheme("https")
+                            .authority("maps.googleapis.com")
+                            .appendPath("maps")
+                            .appendPath("api")
+                            .appendPath("staticmap")
+                            .appendQueryParameter("center", diaModel.getLatitud() + "," + diaModel.getLongitud())
+                            .appendQueryParameter("zoom", "19")
+                            .appendQueryParameter("scale", "false")
+                            .appendQueryParameter("format", "png")
+                            .appendQueryParameter("key", "AIzaSyCDb3AWOIzMkm-_tdCezft9_ygxrgZ3__0")
+                            .appendQueryParameter("markers", "color:orange|" +
+                                    "label:T|"+ diaModel.getLatitud() + "," +
+                                    diaModel.getLongitud())
+                            .appendQueryParameter("size", "500x800");
+
+                    String url = builder.build().toString();
 
                         /*String uri = "geo:" + diaModel.getLatitud() + ","
                                 + diaModel.getLongitud() + "?q=" + diaModel.getLatitud()
                                 + "," + diaModel.getLongitud();*/
 
-                        //Log.d("url", url);
+                    //Log.d("url", url);
 
-                        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                        //intent.setType("image/png");
-                        //context.startActivity(intent);
+                    //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    //intent.setType("image/png");
+                    //context.startActivity(intent);
 
 
-                        //Toast.makeText(context, "pulsaste", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(context, "pulsaste", Toast.LENGTH_SHORT).show();
 
-                        UbicacionDialogFragment dialog =
-                                UbicacionDialogFragment.getInstance(url,diaModel.getLatitud(),
-                                        diaModel.getLongitud());
+                    UbicacionDialogFragment dialog =
+                            UbicacionDialogFragment.getInstance(url,diaModel.getLatitud(),
+                                    diaModel.getLongitud());
 
-                        FragmentTransaction ft = ((Activity) context).getFragmentManager().beginTransaction();
-                        Fragment prev = ((Activity)context).getFragmentManager().findFragmentByTag("dialog");
-                        if (prev != null){
-                            ft.remove(prev);
-                        }
-
-                        ft.addToBackStack(null);
-
-                        dialog.show(ft, "dialog");
-
+                    FragmentTransaction ft = ((Activity) context).getFragmentManager().beginTransaction();
+                    Fragment prev = ((Activity)context).getFragmentManager().findFragmentByTag("dialog");
+                    if (prev != null){
+                        ft.remove(prev);
                     }
-                });
+
+                    ft.addToBackStack(null);
+
+                    dialog.show(ft, "dialog");
+
+                }
+            });
 
 
-            }else {
-                dia.ubicacion.setVisibility(View.INVISIBLE);
-            }
-
-
-
-
-
+        }else {
+            dia.ubicacion.setVisibility(View.INVISIBLE);
         }
+
+
+
     }
 
     @Override
